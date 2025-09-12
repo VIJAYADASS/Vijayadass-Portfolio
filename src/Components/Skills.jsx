a08987b1140fb3eb58f79766c90cd971dd684350
project1 <Vijaydass0702@gmail.com>
2025-09-12 19:09:33 +0530

    update the source path

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,13 +4,13 @@ React;
 
 const Skills = () => {
   const skills = [
-    { name: "HTML", image: "./assets/HTML.png" },
-    { name: "CSS", image: "./assets/CSS.png" },
-    { name: "JavaScript", image: "./assets/Javascript.png" },
-    { name: "React.js", image: "./assets/Reactjs.png" },
-    { name: "Git", image: "./assets/Git.png" },
-    { name: "GitHub", image: "./assets/Github.png" },
-    { name: "Tailwind", image: "./assets/Tailwind.png" },
+    { name: "HTML", image: "./src/assets/HTML.png" },
+    { name: "CSS", image: "./src/assets/CSS.png" },
+    { name: "JavaScript", image: "./src/assets/Javascript.png" },
+    { name: "React.js", image: "./src/assets/Reactjs.png" },
+    { name: "Git", image: "./src/assets/Git.png" },
+    { name: "GitHub", image: "./src/assets/Github.png" },
+    { name: "Tailwind", image: "./src/assets/Tailwind.png" },
   ];
   return (
     <section id="skills" className="py-16 px-8 pt-50 bg-gray-900 text-center">
